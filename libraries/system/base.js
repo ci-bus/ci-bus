8e063a40aab5ff5829cf03908e76c1e6b74a1b4d
Miguel Angel <m.calero@w-b-s.net>
2018-08-07 11:27:26 +0200

    up

--- libraries/system/base.js
+++ libraries/system/base.js
@@ -2207,7 +2207,10 @@ cb.module.bootstrapComponent = {
 		var ele = document.createElement('div');
 		$(ele).addClass('bs-callout');
 		if (!opt.overflow) {
-			$(ele).css('overflow-x', 'auto');
+			$(ele).css({
+			    overflow: 'initial',
+			    'overflow-x': 'auto'
+			});
 		}
 		if (opt.type) $(ele).addClass('bs-callout-'+opt.type);
 		opt.notype = true;
